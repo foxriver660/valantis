f668bf079d16e9c1fe0feab117356da1d4437f0e
foxriver <foxriver660@gmail.com>
2024-03-01 12:40:26 +0300

    fix: change url

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,3 @@
-export const API_URL = 'http://api.valantis.store:40000'
+export const API_URL = 'https://api.valantis.store:41000'
 export const ITEMS_PER_PAGE = 50
 export const DEBOUNCE_DELAY = 1200
